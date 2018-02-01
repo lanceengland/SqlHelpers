13553b3e7c703716afd3df7214b1b520df13ca58
lanceengland <lance.england@gmail.com>
2018-02-01 18:21:05 -0500

    Removed stray comment block from PS1 file

--- MERGE.ps1
+++ MERGE.ps1
@@ -1,12 +1,4 @@
-﻿<#
-.SYNOPSIS –a brief explanation of what the script or function does.
-.DESCRIPTION – a more detailed explanation of what the script or function does.
-.PARAMETER name – an explanation of a specific parameter. Replace name with the parameter name. You can have one of these sections for each parameter the script or function uses.
-.EXAMPLE – an example of how to use the script or function. You can have multiple .EXAMPLE sections if you want to provide more than one example.
-.NOTES – any miscellaneous notes on using the script or function.
-.LINK – a cross-reference to another help topic; you can have more than one of these. If you include a URL beginning with http:// or https://, the shell will open that URL when the Help command’s –online parameter is used.
-#>
-function Get-SQLMergeStatement {
+﻿function Get-SQLMergeStatement {
     <#
         .SYNOPSIS
             Generates and returns T-SQL MERGE statement.
